24b9ef7b06e33a83da51849e31c58cff1ef33371
GeoGiak <ggia@adaptit.gr>
2025-01-24 15:37:54 +0200

    Added Api Interface file

---
+++ Training.JobVacancy.WebModels/IFeedApi.cs
@@ -0,0 +1,17 @@
+﻿using Adaptit.Training.JobVacancy.Backend.Dto;
+
+using Refit;
+
+namespace Adaptit.Training.JobVacancy.WebModels;
+
+public interface IFeedApi
+{
+	[Get("/api/v1/feed")]
+	Task<ApiResponse<Feed>> GetAllFeed(string? last);
+
+	[Get("/api/v1/feed/{feedPageId:int)")]
+	Task<ApiResponse<Feed>> GetFeed(int feedPageId);
+
+	[Get("/api/v1/feedentry/{entryId:int}")]
+	Task<ApiResponse<FeedEntry>> GetFeedEntry(int entryId);
+}
