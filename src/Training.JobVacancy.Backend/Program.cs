82df72ef097bbcd93f028aee6e08f27693facce1
GeoGiak <ggia@adaptit.gr>
2025-01-24 15:31:02 +0200

    Setted Refit Http Client

--- src/Training.JobVacancy.Backend/Program.cs
+++ src/Training.JobVacancy.Backend/Program.cs
@@ -1,12 +1,21 @@
 using Adaptit.Training.JobVacancy.Backend.Endpoints;
+using Adaptit.Training.JobVacancy.WebModels;
 
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+using Refit;
 
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<OpenIdConnectOptions>();
+builder.Services.AddRefitClient<IFeedApi>()
+	.ConfigureHttpClient(c =>
+	{
+		c.BaseAddress = new Uri("https://pam-stilling-feed.nav.no");
+	});
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
