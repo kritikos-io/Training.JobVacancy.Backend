a5969f4419f08321751bec35682fbab645a5b67f
Alexandros Kritikos <akritikos@outlook.com>
2025-01-28 23:06:35 +0200

    📌 Sanitizes nuget dependencies

--- src/Training.JobVacancy.Backend/Dto/WeatherForecast.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Adaptit.Training.JobVacancy.Backend.Dto;
-
-public record WeatherForecast(int Id, DateOnly Date, int TemperatureC, string? Summary)
-{
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
