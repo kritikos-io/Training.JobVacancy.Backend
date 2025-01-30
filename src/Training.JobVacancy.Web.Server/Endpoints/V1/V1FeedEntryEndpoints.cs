75c16bed75dd965ff049b354ac6713997eddcc83
Alexandros Kritikos <akritikos@outlook.com>
2025-01-30 15:56:12 +0200

    👽️ Adds sample endpoints according to NAV feed

---
+++ src/Training.JobVacancy.Web.Server/Endpoints/V1/V1FeedEntryEndpoints.cs
@@ -0,0 +1,35 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Server.Endpoints.V1;
+
+using Adaptit.Training.JobVacancy.Backend.Helpers;
+using Adaptit.Training.JobVacancy.Web.Models.Dto.NavJobVacancy;
+using Adaptit.Training.JobVacancy.Web.Server.Repositories;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public class V1FeedEntryEndpoints
+{
+  public static RouteGroupBuilder Map(RouteGroupBuilder endpoint)
+  {
+    var group = endpoint.MapGroup("feedentry")
+        .WithTags("Feed");
+
+    group.MapGet("{id:guid}", GetFeedEntry);
+
+    return endpoint;
+  }
+
+  public static Results<Ok<EntryDto>, NotFound> GetFeedEntry(
+      Guid id,
+      NavJobVacancyRepo repository,
+      ILogger<V1FeedEntryEndpoints> logger)
+  {
+    var entry = repository.Entries.FirstOrDefault(x => x.Uuid == id);
+    if (entry is null)
+    {
+      logger.LogEntityNotFound(nameof(EntryDto), id);
+      return TypedResults.NotFound();
+    }
+
+    return TypedResults.Ok(entry);
+  }
+}
