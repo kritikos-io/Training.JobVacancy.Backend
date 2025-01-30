a2d75d480769afdcf2e3f0a3d0e3f28de4600ff6
Alexandros Kritikos <akritikos@outlook.com>
2025-01-30 15:57:07 +0200

    🎨 Moves DI & Middlware configuration to their own files

--- src/Training.JobVacancy.Web.Server/ConfigureServices.cs
+++ src/Training.JobVacancy.Web.Server/ConfigureServices.cs
@@ -3,6 +3,9 @@
 using Adaptit.Training.JobVacancy.Data;
 using Adaptit.Training.JobVacancy.Web.Models;
 using Adaptit.Training.JobVacancy.Web.Server.Options;
+using Adaptit.Training.JobVacancy.Web.Server.Repositories;
+
+using Asp.Versioning;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -13,10 +16,40 @@ public static class ConfigureServices
 {
   public static void AddJobVacancyServices(this WebApplicationBuilder builder)
   {
+    builder.Services.AddProblemDetails();
+
     builder.AddNavJobVacancyClient();
+    builder.AddNavJobRepository();
+
+    builder.AddApiDocumentation();
+    builder.AddApiVersioning();
+
     builder.Services.AddDbContext<JobVacancyDbContext>(options => options
         .UseNpgsql(builder.Configuration.GetConnectionString("JobVacancyDatabase"))
         .EnableSensitiveDataLogging());
+  }
+
+  public static void AddApiDocumentation(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddOpenApi();
+  }
+
+  public static void AddApiVersioning(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddApiVersioning(options =>
+        {
+          options.DefaultApiVersion = new ApiVersion(1);
+          options.ReportApiVersions = true;
+          options.AssumeDefaultVersionWhenUnspecified = true;
+          options.ApiVersionReader = new UrlSegmentApiVersionReader();
+        })
+        .AddApiExplorer(options =>
+        {
+          options.GroupNameFormat = "'v'V";
+          options.SubstituteApiVersionInUrl = true;
+          options.AssumeDefaultVersionWhenUnspecified = true;
+          options.RouteConstraintName = "apiVersion";
+        });
   }
 
   public static void AddNavJobVacancyClient(this WebApplicationBuilder builder)
@@ -35,5 +68,14 @@ public static class ConfigureServices
           var settings = sp.GetRequiredService<IOptions<NavJobVacancyOptions>>().Value;
           client.BaseAddress = new Uri(settings.BaseAddress.ToString());
         });
+  }
+
+  public static void AddNavJobRepository(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddSingleton<NavJobVacancyRepo>(options =>
+    {
+      var seed = builder.Configuration.GetValue<int>("Seed", 32131095);
+      return new NavJobVacancyRepo(seed);
+    });
   }
 }
