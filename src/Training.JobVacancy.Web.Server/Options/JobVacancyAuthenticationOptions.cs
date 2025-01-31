39e0f49a95cc3bf6a4c3c8559368a8608716b080
Alexandros Kritikos <akritikos@outlook.com>
2025-01-31 16:38:48 +0200

    🛂 Adds JWT Bearer Authentication configuration

---
+++ src/Training.JobVacancy.Web.Server/Options/JobVacancyAuthenticationOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Server.Options;
+
+using System.ComponentModel.DataAnnotations;
+
+public class JobVacancyAuthenticationOptions
+{
+  public const string Section = "Authentication:OpenId";
+
+  [Required(AllowEmptyStrings = false)]
+  [Url]
+  public string Authority { get; set; }
+
+  public string ClientId { get; set; } = string.Empty;
+
+  public string Audience { get; set; } = "account";
+}
