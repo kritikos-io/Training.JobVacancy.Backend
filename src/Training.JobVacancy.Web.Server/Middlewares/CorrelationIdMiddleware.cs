bf34b86c1b49b6f4d3b7748307fe36e7f4bb65a3
GeoGiak <ggia@adaptit.gr>
2025-01-31 11:54:57 +0200

    Initial Commit

---
+++ src/Training.JobVacancy.Web.Server/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,10 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Server.Middlewares;
+
+public class CorrelationIdMiddleware : IMiddleware
+{
+  /// <inheritdoc />
+  public Task InvokeAsync(HttpContext context, RequestDelegate next)
+  {
+    return next.Invoke(context);
+  }
+}
