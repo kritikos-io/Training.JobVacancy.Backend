ef87e6d122a3fdb75cd342eaa2310cda7c93ca9f
Yannis <jdkakouris@gmail.com>
2025-02-02 11:29:08 +0200

    Added user class and dbset

--- src/Training.JobVacancy.Data/JobVacancyDbContext.cs
+++ src/Training.JobVacancy.Data/JobVacancyDbContext.cs
@@ -1,8 +1,11 @@
 ﻿namespace Adaptit.Training.JobVacancy.Data;
 
+using Adaptit.Training.JobVacancy.Data.Entities;
+
 using Microsoft.EntityFrameworkCore;
 
 public class JobVacancyDbContext(DbContextOptions<JobVacancyDbContext> options)
     : DbContext(options)
 {
+  public DbSet<User> Users { get; set; }
 }
