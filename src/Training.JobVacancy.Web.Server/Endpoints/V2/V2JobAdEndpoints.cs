d228eb34d352b5bc109c3f854bf233cb1cc9389b
GeoGiak <ggia@adaptit.gr>
2025-02-05 13:54:13 +0200

    Created blueprint of endpoints for job ads.

---
+++ src/Training.JobVacancy.Web.Server/Endpoints/V2/V2JobAdEndpoints.cs
@@ -0,0 +1,31 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Server.Endpoints.V2;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public class V2JobAdEndpoints
+{
+  public static RouteGroupBuilder Map(RouteGroupBuilder endpoint)
+  {
+    var group = endpoint.MapGroup("jobad")
+          .WithTags("JobAd");
+
+    group.MapGet("", GetAllJobAds);
+    group.MapGet("{id:guid}", GetJobAd);
+
+    group.MapPost("", CreateJobAd);
+    group.MapPatch("{id:guid}", UpdateJobAd);
+    group.MapDelete("{id:guid}", DeleteJobAd);
+
+    return endpoint;
+  }
+
+  public static Results<Ok, NotFound> GetAllJobAds() => throw new NotImplementedException();
+
+  public static Results<Ok, NotFound> GetJobAd(Guid id) => throw new NotImplementedException();
+
+  public static Results<Created<Object>, BadRequest<string>> CreateJobAd(Object ad) => throw new NotImplementedException();
+
+  public static Results<Ok<string>, NotFound<string>> UpdateJobAd(Guid id) => throw new NotImplementedException();
+
+  public static Results<Ok<string>, NotFound<string>> DeleteJobAd(Guid id) => throw new NotImplementedException();
+}
