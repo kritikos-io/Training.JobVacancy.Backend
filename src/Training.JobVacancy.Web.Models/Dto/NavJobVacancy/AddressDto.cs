20660f6f4ac23f13984b36165f9a518a8a58822a
Nikolaos Bourtzoglou <nbou@adaptit.com>
2025-02-06 16:46:58 +0200

    Created DTOs for company, address.

---
+++ src/Training.JobVacancy.Web.Models/Dto/NavJobVacancy/AddressDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Models.Dto.NavJobVacancy;
+
+public class AddressDto
+{
+  public string Country { get; set; } = string.Empty;
+
+  public string City { get; set; } = string.Empty;
+
+  public string Street { get; set; } = string.Empty;
+
+  public string StreetNumber { get; set; } = string.Empty;
+
+  public string Postcode { get; set; } = string.Empty;
+}
---
+++ src/Training.JobVacancy.Web.Models/Dto/NavJobVacancy/CompanyDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Models.Dto.NavJobVacancy;
+
+public class CompanyDto
+{
+  public Guid Id { get; set; }
+
+  public string Name { get; set; } = string.Empty;
+
+  public Uri Website { get; set; } = new Uri(string.Empty);
+
+  public string Vat { get; set; } = string.Empty;
+
+  public Uri LogoUrl { get; set; } = new Uri(string.Empty);
+
+  public AddressDto Address { get; set; } = new AddressDto();
+
+  public bool Sponsored { get; set; } = false;
+
+  public int TotalJobsAdvertised { get; set; } = 0;
+
+}
