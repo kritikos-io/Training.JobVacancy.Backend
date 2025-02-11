a1ff34c2387a8085e64a3435311710dfb53bfdb3
GeoGiak <ggia@adaptit.gr>
2025-02-11 13:58:05 +0200

    Created extentions for transforming dto to entity and the other way around.

---
+++ src/Training.JobVacancy.Web.Server/Extensions/EntityDtoTransformations.cs
@@ -0,0 +1,31 @@
+﻿namespace Adaptit.Training.JobVacancy.Web.Server.Extensions;
+
+using Adaptit.Training.JobVacancy.Data.Entities;
+using Adaptit.Training.JobVacancy.Web.Models.Dto.V2;
+
+public static class EntityDtoTransformations
+{
+  public static JobAd ToDto(this JobAdDto dto) => new()
+  {
+    Id = Guid.NewGuid(),
+    Type = dto.Type,
+    SalaryRange = dto.SalaryRange,
+    Description = dto.Description,
+    Location = dto.Location,
+    CreatedAt = dto.CreatedAt,
+    ExpiresAt = dto.ExpiresAt,
+    Level = dto.Level,
+  };
+
+  public static JobAdDto ToEntity(this JobAd jobAd) => new()
+  {
+    Id = jobAd.Id,
+    Type = jobAd.Type,
+    SalaryRange = jobAd.SalaryRange,
+    Description = jobAd.Description,
+    Location = jobAd.Location,
+    CreatedAt = jobAd.CreatedAt,
+    ExpiresAt = jobAd.ExpiresAt,
+    Level = jobAd.Level,
+  };
+}
