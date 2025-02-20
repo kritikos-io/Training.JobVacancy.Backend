a3ff5f9bb0ba47aff1bfac68daf5b037a5e18a4f
Yannis <jdkakouris@gmail.com>
2025-02-20 11:55:03 +0200

    User and Resume Dto adjustments

--- src/Training.JobVacancy.Web.Models/Dto/User/UserReturnDto.cs
+++ src/Training.JobVacancy.Web.Models/Dto/User/UserReturnDto.cs
@@ -1,9 +1,11 @@
 namespace Adaptit.Training.JobVacancy.Web.Models.Dto.User;
 
+using Adaptit.Training.JobVacancy.Web.Models.Dto.Resume;
+
 public class UserReturnDto
 {
   public Guid Id { get; set; }
   public string Name { get; set; } = string.Empty;
   public string Surname { get; set; } = string.Empty;
-  public Uri? Resume { get; set; }
+  public List<ResumeReturnDto> Resumes { get; set; } = [];
 }
