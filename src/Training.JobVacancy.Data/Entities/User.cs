c9cb7f0c87fc66dbecb3b822000369d2dc0922b9
Yannis <jdkakouris@gmail.com>
2025-02-25 15:28:07 +0200

    user adjustment

--- src/Training.JobVacancy.Data/Entities/User.cs
+++ src/Training.JobVacancy.Data/Entities/User.cs
@@ -8,6 +8,7 @@ public class User
 {
   public Guid Id { get; init; }
   public string Name { get; set; } = string.Empty;
-  public string Surname { get; set; } = string.Empty;
+  public string LastName { get; set; } = string.Empty;
+  public string Email { get; set; } = string.Empty;
   public List<Resume> Resumes { get; set; } = [];
 }
